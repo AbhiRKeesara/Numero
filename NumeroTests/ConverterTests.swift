7bb28cc48e85db129d293b7e6d02b2de79dcb308
Abhinay Reddy Keesara <>
2021-09-08 22:50:54 -1000

    new changes test

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -26,4 +26,32 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+  
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+  
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+  
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+  
+  func testConversionForTwo() {
+    let result = converter.convert(2)
+    XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+  }
+  
+}
+
 
